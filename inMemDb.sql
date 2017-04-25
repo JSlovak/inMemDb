4d3367394fc579fa5953e81dc2e9b7086062206e
JSlovak <jasmine.slovak@gmail.com>
2017-04-25 10:04:10 -1000

    Database and queries working.

--- inMemDb.sql
+++ inMemDb.sql
@@ -12,26 +12,26 @@ CREATE DATABASE morning_challenge;
 
 -- Create a table users
 CREATE TABLE IF NOT EXISTS users(
-  id SERIAL PRIMARY KEY
+  id SERIAL PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   age INTEGER NOT NULL,
   gender VARCHAR(1) NOT NULL,
-  jobType VARCHAR(2) NOT NULL
+  job_type VARCHAR(10) NOT NULL
 );
 
 --  Add users to that table
-INSERT INTO users(name, age, gender, jobType) VALUES ('Alice', 12, 'f', 'st');
-INSERT INTO users(name, age, gender, jobType) VALUES ('Bob', 21, 'm', 'dv');
-INSERT INTO users(name, age, gender, jobType) VALUES ('Claire', 56, 'f', 'dv');
-INSERT INTO users(name, age, gender, jobType) VALUES ('David', 9, 'm', 'na');
-INSERT INTO users(name, age, gender, jobType) VALUES ('Eric', 61, 'm', 'dv');
-INSERT INTO users(name, age, gender, jobType) VALUES ('Fred', 45, 'm', 'dv');
-INSERT INTO users(name, age, gender, jobType) VALUES ('George', 38, 'm', 'dv');
-INSERT INTO users(name, age, gender, jobType) VALUES ('Hannah', 15, 'f', 'na');
-INSERT INTO users(name, age, gender, jobType) VALUES ('Ilona', 17, 'f', 'st');
-INSERT INTO users(name, age, gender, jobType) VALUES ('Jake', 27, 'm', 'dv');
-INSERT INTO users(name, age, gender, jobType) VALUES ('Kathy', 8, 'f', 'na');
-INSERT INTO users(name, age, gender, jobType) VALUES ('Liam', 20, 'm', 'st');
+INSERT INTO users(name, age, gender, job_type) VALUES ('Alice', 12, 'f', 'st');
+INSERT INTO users(name, age, gender, job_type) VALUES ('Bob', 21, 'm', 'dv');
+INSERT INTO users(name, age, gender, job_type) VALUES ('Claire', 56, 'f', 'dv');
+INSERT INTO users(name, age, gender, job_type) VALUES ('David', 9, 'm', 'na');
+INSERT INTO users(name, age, gender, job_type) VALUES ('Eric', 61, 'm', 'dv');
+INSERT INTO users(name, age, gender, job_type) VALUES ('Fred', 45, 'm', 'dv');
+INSERT INTO users(name, age, gender, job_type) VALUES ('George', 38, 'm', 'dv');
+INSERT INTO users(name, age, gender, job_type) VALUES ('Hannah', 15, 'f', 'na');
+INSERT INTO users(name, age, gender, job_type) VALUES ('Ilona', 17, 'f', 'st');
+INSERT INTO users(name, age, gender, job_type) VALUES ('Jake', 27, 'm', 'dv');
+INSERT INTO users(name, age, gender, job_type) VALUES ('Kathy', 8, 'f', 'na');
+INSERT INTO users(name, age, gender, job_type) VALUES ('Liam', 20, 'm', 'st');
 
 -- SELECT all users, show their names and ids
 
@@ -42,13 +42,13 @@ FROM users;
 
 SELECT *
 FROM users
-ORDER BY age ASC;
+WHERE age = (SELECT MIN(age) FROM users);
 
 -- SELECT the oldest user
 
 SELECT *
 FROM users
-ORDER BY age DESC;
+WHERE age = (SELECT MAX(age) FROM users);
 
 -- SELECT all users, that are male
 SELECT *
@@ -68,4 +68,4 @@ ORDER BY jobType
 -- SELECT all users, that are above 21
 SELECT *
 FROM users
-WHERE age > 21;
+WHERE age > 2;
